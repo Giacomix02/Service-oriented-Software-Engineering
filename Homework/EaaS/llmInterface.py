28cd00338577a422b8d56692b5a6cc90ae15f068
Giacomix02 <paoloccigiacomo@gmail.com>
2026-05-22 22:34:57 +0200

    llmInterface.py created singleton form env

--- Homework/EaaS/llmInterface.py
+++ Homework/EaaS/llmInterface.py
@@ -1,15 +1,24 @@
 import os
 from dotenv import load_dotenv
 
+class Env:
+    _instance = None
+    token = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Env, cls).__new__(cls)
+            load_dotenv()
+            cls.token = os.getenv('GEMINI_TOKEN')
+        return cls._instance
 
 # here is the main core of the application
 # llm token is putted in .env file
-# to load env:
-#
-# load_dotenv()
-# GEMINI_TOKEN = os.getenv('GEMINI_TOKEN')
-# probably for the env is better to create a singleton to not load it every time
 
 def consultLlm():
+    env = Env()
+    geminiToken = env.token
+
+
 
     return 0
