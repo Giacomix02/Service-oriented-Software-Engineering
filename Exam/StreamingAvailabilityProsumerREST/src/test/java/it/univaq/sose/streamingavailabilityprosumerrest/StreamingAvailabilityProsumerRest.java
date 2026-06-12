50b2377ae64c4dac9d96ca93c91c0c10855cfc0f
Giacomix02 <paoloccigiacomo@gmail.com>
2026-06-12 23:17:38 +0200

    new prosumer, updated doc

---
+++ Exam/StreamingAvailabilityProsumerREST/src/test/java/it/univaq/sose/streamingavailabilityprosumerrest/StreamingAvailabilityProsumerRest.java
@@ -0,0 +1,13 @@
+package it.univaq.sose.streamingavailabilityprosumerrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingAvailabilityProsumerRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
