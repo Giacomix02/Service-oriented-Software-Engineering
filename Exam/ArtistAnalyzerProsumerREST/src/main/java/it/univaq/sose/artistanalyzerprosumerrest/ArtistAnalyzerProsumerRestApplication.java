f75f596429037a08f92bf93f7292a5a95084546a
Giacomix02 <paoloccigiacomo@gmail.com>
2026-06-23 20:26:40 +0200

    finish load balancer, exposed swagger doc via gateway, load balancer client added in ArtistAnalyzerProsumerRestApplication

--- Exam/ArtistAnalyzerProsumerREST/src/main/java/it/univaq/sose/artistanalyzerprosumerrest/ArtistAnalyzerProsumerRestApplication.java
+++ Exam/ArtistAnalyzerProsumerREST/src/main/java/it/univaq/sose/artistanalyzerprosumerrest/ArtistAnalyzerProsumerRestApplication.java
@@ -2,8 +2,12 @@ package it.univaq.sose.artistanalyzerprosumerrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ArtistAnalyzerProsumerRestApplication {
 
 	public static void main(String[] args) {
